d11abf0b699c37bf22387307cc89209915639904
citarf <citarf@pop-os.localdomain>
2023-03-05 16:56:28 +0100

    first commit

---
+++ src/main/java/org/example/TokenType.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public enum TokenType {
+
+    // Single characters tokens
+    LPAREN, RPAREN, LBRACE, RBRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON,
+    SLASH, STAR,
+
+    // One or two character token.
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIERS, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
